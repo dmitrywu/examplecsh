353ff10a0d5136f04693802d0f6a4be943e2224d
Dmitry <dmitry.wu@gmail.com>
2023-09-17 11:40:23 +0300

    random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,3 +1,8 @@
-﻿int NumberA = 3;
-int NumberB = 5;
-Console.Write (NumberA + NumberB);
+﻿int NumberA = new Random().Next(1,150); // 1 2 3 4 5 6 7 8 9
+int NumberB = new Random().Next(1,150);
+int result = NumberA + NumberB;
+Console.Write(NumberA);
+Console.Write("+");
+Console.Write(NumberB);
+Console.Write("=");
+Console.Write(result);
